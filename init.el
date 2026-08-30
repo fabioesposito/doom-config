758f58ea76d4afebed67980e069930e8b0b805bf
Fabio E. <fabio.esposito@spendone.com>
2026-08-30 12:33:44 +0200

    add lexical-binding cookie to init.el

--- init.el
+++ init.el
@@ -1,3 +1,5 @@
+;;; init.el -*- lexical-binding: t; -*-
+
 ;; ── Package bootstrap ───────────────────────────────────────────────
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -86,3 +88,17 @@
 
 ;; ── Savehist ── persist minibuffer history across sessions ─────────
 (savehist-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(consult embark embark-consult evil magit marginalia orderless
+	     projectile vertico)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
